a3fb111afea48255983f031d586b2c6f53406902
Ilhaamaham <110713163+Ilhaamaham@users.noreply.github.com>
2022-10-10 17:21:46 +0530

    Add files via upload

---
+++ javascript/info.js
@@ -0,0 +1,21 @@
+function func1()
+{
+    var x = document.getElementById("num");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+
+}
+
+function func2()
+{
+    var x = document.getElementById("instaid");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+
+}
